eb8171137ed61f30c47a7d279595615530f5b34f
Jan <jerech.curan@urios.edu.ph>
2026-04-21 14:02:29 +0800

    Adding PORT

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.post('/upload', upload.single('image'), async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
